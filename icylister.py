adf3ff7db131694661cd1fdd8b227dbece979723
Loetkolben <sirloetkolben@gmail.com>
2017-06-05 13:47:48 +0200

    Partial rewrite, rm BAD_ARTISTS and replace with re-based blacklist

--- icylister.py
+++ icylister.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 
 from urllib.request import Request, urlopen
 from datetime import datetime
@@ -8,18 +8,19 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def get_stream(stream_url):
+def get_stream(stream_url, user_agent="VLC/2.2.4 LibVLC/2.2.4"):
     """
-    Gets the stream, pretending to be VLC media player (via User-Agent header).
+    Gets the stream by performing the appropriate HTTP request.
     Args:
         stream_url: HTTP URL where the stream can be found
+        user_agent: Optional. User-Agent to supply to the server. Defaults to 'VLC/2.2.4 LibVLC/2.2.4'
 
     Returns:
         The data stream from the server as obtained by `urllib.request.urlopen(...)`.
         Has the special attribute `metaint` which is the mp3 meta interval.
     """
     request = Request(stream_url, headers={"Icy-MetaData": "1",
-                                           "User-Agent": "VLC/2.2.4 LibVLC/2.2.4"})
+                                           "User-Agent": user_agent})
     stream = urlopen(request)
 
     for header, value in stream.getheaders():
@@ -28,32 +29,12 @@ def get_stream(stream_url):
     try:
         meta_interval
     except NameError:
-        print("Server did NOT respond with a \'Icy-Metaint\' header, cannot extract metadata!")
-        print("Are you sure the server we're talking to is a Icecast (... compatible thingy)?")
-        meta_interval = None
+        raise RuntimeError("Server did NOT respond with a \'Icy-Metaint\' header. "
+                           "This makes it impossible to extract metadata (we do not know which frames hold metadata). "
+                           "Are you sure the server we're talking to is a Icecast (... compatible thingy)?")
 
     stream.metaint = meta_interval
-
     return stream
-
-
-def get_metadata_once(stream, metaint):
-    """
-    Read `metaint` bytes from the stream, get and parse the metadata.
-    Args:
-        stream: stream from `get_stream(...)`
-        metaint: metaint fron `get_stream(...)`
-
-    Returns:
-        The parsed metadata as a dict (key => value) (sometimes potentially unescaped), if available.
-        Returns `None` if metadata length was specified as 0.
-    """
-    stream.read(metaint)  # Eat the useless mp3 data
-    meta_len = int(stream.read(1).hex(), 16) * 16  # This just feels so wrong. There has to be a better way...
-    if meta_len > 0:
-        meta_data = stream.read(meta_len).replace(b"\x00", b"")
-        meta_data = meta_data.decode("Windows-1252")  # Default western code page? Seems to work, so...
-        return parse_icy_metadata(meta_data)
 
 
 def parse_icy_metadata(data_string):
@@ -77,9 +58,10 @@ def parse_icy_metadata(data_string):
             else:
                 current_tag += char
         else:  # Currently reading value field
-            if char == "'":
-                pass  # I hope this works
-            elif char == ';':
+            if char == ';':
+                if current_value[0] == "'" and current_value[-1] == "'":
+                    # Cut away the "'" at the beginning and end if they exist.
+                    current_value = current_value[1:-1]
                 metadata[current_tag] = current_value
                 current_tag = ""
                 current_value = ""
@@ -89,50 +71,61 @@ def parse_icy_metadata(data_string):
     return metadata
 
 
-def print_stream_titles(stream_url, bad_artists=[], bad_to_stderr=True):
+def get_metadata_once(stream, metaint):
     """
-    Prints `Current Time <TAB> StreamTitle as Artist ~ Song title` as acquired from stream metadata to STDOUT,
-    until interrupted by Ctrl+C. Unwanted artists may and errors will be printed to STDERR.
+    Read `metaint` bytes from the stream, get and parse the metadata.
+    Args:
+        stream: stream from `get_stream(...)`
+        metaint: metaint fron `get_stream(...)`
+
+    Returns:
+        The parsed metadata as a dict (key => value) (sometimes potentially unescaped), if available.
+        Returns `None` if metadata length was specified as 0.
+    """
+    stream.read(metaint)  # Eat the useless mp3 data
+    meta_len = int(stream.read(1).hex(), 16) * 16  # This just feels so wrong. There has to be a better way...
+    if meta_len > 0:
+        meta_data = stream.read(meta_len).replace(b"\x00", b"")
+        meta_data = meta_data.decode("Windows-1252")  # Default western code page? Seems to work, so...
+        return parse_icy_metadata(meta_data)
+
+
+def print_stream_titles(stream_url, title_blacklist=[]):
+    """
+    Prints `Current Time <TAB> StreamTitle` as acquired from stream metadata to STDOUT,
+    until interrupted by Ctrl+C. Errors will be printed to STDERR.
     Args:
         stream_url: Stream URL
-        bad_artists: A list of bad, unwanted artists. Will be hidden or printed to STDERR depending on `bad_to_stderr`.
-        bad_to_stderr: Print 'bad' unwanted artists to STDERR (True) or skip them (False)
+        title_blacklist: A list of compiled regexes that are used to filter out (-> print to STDERR) unwanted titles.
     """
     stream = get_stream(stream_url)
     meta_interval = stream.metaint
 
     try:
         while True:
-            meta_data_parsed = get_metadata_once(stream, meta_interval)
-            if meta_data_parsed is not None:
-                if "StreamTitle" in meta_data_parsed:
-                    try:
-                        artist, song = meta_data_parsed["StreamTitle"].split(" - ", 1)
-                        if artist not in bad_artists:
-                            print(str(datetime.now()) + "\t" + artist + " ~ " + song)
-                        elif bad_to_stderr:
-                            eprint(str(datetime.now()) + "\t" + artist + " ~ " + song)
-                    except ValueError:  # Unpack error
-                        eprint(str(datetime.now()) + "\t" + "StreamTitle bad(?):" + meta_data_parsed["StreamTitle"])
+            meta_data = get_metadata_once(stream, meta_interval)
+            if meta_data is not None:
+                if "StreamTitle" in meta_data:
+                    is_blacklisted = False
+                    for bl_entry in title_blacklist:
+                        if bl_entry.match(meta_data["StreamTitle"]):
+                            is_blacklisted = True
+                    if is_blacklisted:
+                        eprint(str(datetime.now()) + "\t" + meta_data["StreamTitle"])
+                    else:
+                        print(str(datetime.now()) + "\t" + meta_data["StreamTitle"])
                 else:
-                    eprint(str(datetime.now()) + "\t" + str(meta_data_parsed))
+                    eprint(str(datetime.now()) + "\t" + "No 'StreamTitle' in metadata: " + str(meta_data))
     except KeyboardInterrupt:
         stream.close()
 
 
 if __name__ == "__main__":
-    argv = sys.argv
-    if len(argv) < 2:
-        print("ERR: Bad arguments")
-        print("Usage: $ py -3 icylister.py <STREAM_URL> [BAD_ARTIST]")
-        print("Depending on your shell / cmd / whatever, STREAM_URL may need to be escaped and / or quoted.")
-        print("BAD_ARTIST can be repeated to list multiple artists. It is case-sensitive")
-        sys.exit(1)
+    url = sys.argv[1]
 
-    url = argv[1]
-    bad = argv[2:] if len(argv) >= 2 else []
+    blacklist_str = sys.argv[2:]
+    blacklist_re = []
+    for entry in blacklist_str:
+        blacklist_re.append(re.compile(entry))
 
-    eprint("# Stream URL : " + url)
-    eprint("# Bad Artists : " + str(bad))
-
-    print_stream_titles(url, bad)
+    print_stream_titles(url, blacklist_re)
